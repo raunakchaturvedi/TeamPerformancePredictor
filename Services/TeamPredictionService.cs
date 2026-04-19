ea463b176c7c0125f40d587a7e2b20195ff61fc9
raunakchaturvedi <raunakchaturvedi17@gmail.com>
2026-04-19 23:05:27 +0530

    Updated multiple files

---
+++ Services/TeamPredictionService.cs
@@ -0,0 +1,27 @@
+﻿namespace TPPProject.Services
+{
+    public class TeamPredictionService
+    {
+        public int CalculateTeamScore(
+            int highPerformance,
+            int mediumPerformance,
+            int highBurnout,
+            int mediumBurnout)
+        {
+            return (highPerformance * 2)
+                 + (mediumPerformance * 1)
+                 - (highBurnout * 2)
+                 - (mediumBurnout * 1);
+        }
+
+        public string GetTeamPerformanceLevel(int teamScore)
+        {
+            if (teamScore >= 2)
+                return "High";
+            else if (teamScore >= 0)
+                return "Medium";
+            else
+                return "Low";
+        }
+    }
+}
